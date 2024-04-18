e1eebb07be6c2698f3a40f3a48f9883aee4900f4
APOLOWILKER <wilkerapolocarvalho@gmail.com>
2024-04-18 17:23:23 -0300

    style: create structure of post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,11 +1,41 @@
-export function Post ({author, content})  {
+import styles from './Post.module.css'
+
+export function Post ()  {
 
   return (
-    <div>
-      <strong>
-        <p>{author}</p>
-      </strong>
-      <p>{content}</p>
-    </div>
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img
+            className={styles.avatar}
+           src="https://github.com/APOLOWILKER.png"
+          />
+          <div className={styles.authorInfo}>
+            <strong>Apolo Wilker</strong>
+            <span>Web Development</span>
+          </div>
+        </div>
+        <time title="18 de Abril às 17:00h" dateTime="2024-04-18">
+          Publicado a 1h
+        </time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+
+
+        <p>Acabei de subir mais um projeto no meu portifa.
+        É um projeto que fiz no NLW Return, evento da Rocketseat.
+        O nome do projeto é DoctorCare 🚀</p>
+
+        <p>👉<a href="#"> jane.design/doctorcare</a></p>
+
+        <p>
+          <a href="#">#novoprojeto</a>
+          <a href="#">#nlw</a>
+          <a href="#">#rocketseat</a>
+        </p>
+      </div>
+    </article>
   )
 }
